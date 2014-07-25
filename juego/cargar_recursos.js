816ee2a0866d852014cf30d8343794750f5d15b4
unknown <Hubber@.(none)>
2014-07-25 16:49:53 -0500

    boton de pausa con sonido

--- juego/cargar_recursos.js
+++ juego/cargar_recursos.js
@@ -1,7 +1,7 @@
 //D66
 //indicar en una cadena los recursos: audios, imagenes, tmx, json
 //callback para configurar los sprites
-var recursos = "bump.ogg, patada.mp3, salto_enano.mp3, tema_superficie.mp3, jugador.json, mundo1_terminado.tmx, mosaicos_escenario_32x32.png, mosaicos_mario_enano_30x30.png, enemigosBajos.json, mosaicos_enemigos_32x32.png, tortuga.json, mosaicos_enemigos_32x46.png";
+var recursos = "pausa.mp3, bump.ogg, patada.mp3, salto_enano.mp3, tema_superficie.mp3, jugador.json, mundo1_terminado.tmx, mosaicos_escenario_32x32.png, mosaicos_mario_enano_30x30.png, enemigosBajos.json, mosaicos_enemigos_32x32.png, tortuga.json, mosaicos_enemigos_32x46.png";
 
 Q.loadTMX(recursos, function() {
 	//se ejecuta hasta que los recursos estén listos
@@ -36,16 +36,28 @@ $("#boton-pausa").click(function() {
 
 	//si el juego esta pausado y el usuario presiona el boton reanudamos el jego
 	if (Q.pausado === true) {
-		
+		Q.audio.play("pausa.mp3");
+		//reanuda el juego
 		Q.stage(0).unpause();
+		
+		//reanudamos el audio
+		//en  este momento el frame de quintus no nos permite reanudar la cancion 
+		//en el momento en el que se pausó
+		Q.audio.play("tema_superficie.mp3");
 		
 		esteBoton.html("Pausar");
 		Q.pausado = false;
 	} else {
 		//el juego está corriendo y el usuario presiona pausar
 		Q.stage(0).pause();
+		//a continuación detenemos el audio
+		//le podemos pasar como parámetro el nombre de la pista que queremos pausar,
+		//si tenemos varias, si no le pasamos nada, detiene todas
+		Q.audio.stop("tema_superficie.mp3");
+		
 		//el método html cambia el contenido de una etiqueta html
 		esteBoton.html("Reanudar");
+		Q.audio.play("pausa.mp3");
 		Q.pausado = true;
 	}
 
